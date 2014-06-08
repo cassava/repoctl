b8b52fae400377bca76f4755f529479bab8740a4
Ben Morgan <neembi@gmail.com>
2014-06-08 09:15:08 +0200

    Improving error handling in directory reading functions
    We no longer just print messages; rather we send the errors back.

--- fs.go
+++ fs.go
@@ -5,26 +5,49 @@
 package pacman
 
 import (
-	"log"
+	"errors"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-// GetAllPackages takes a directory path as an argument, and
-// then reads all the package information into a list.
-func ReadDir(dirpath string) []*Package {
+// ReadDir reads all the packages it finds in a directory, recursing into
+// subdirectories.
+//
+// Errors are passed through the channel if the channel is not nil, otherwise
+// they are ignored. Make sure you handle the errors right away, like so:
+//
+//	ch := make(chan error)
+//	go func() {
+//		for {
+//			err, open := <-ch
+//			if !open {
+//				break
+//			}
+//			fmt.Println("error:", err)
+//		}
+//	}()
+//	pkgs := ReadDir(dirpath, ch)
+//	close(ch)
+//
+// Because if you don't, the program will probably run into a deadlock when
+// there is an error.
+func ReadDir(dirpath string, ch chan<- error) []*Package {
 	var pkgs []*Package
 
 	filepath.Walk(dirpath, func(filename string, info os.FileInfo, err error) error {
 		if err != nil {
-			log.Printf("Warning: %s\n", err)
+			if ch != nil {
+				ch <- err
+			}
 			return nil
 		}
 		if !info.Mode().IsDir() && HasPackageFormat(filename) {
 			p, err := ReadPackage(filename)
 			if err != nil {
-				log.Printf("Warning: %s\n", err)
+				if ch != nil {
+					ch <- err
+				}
 				return nil
 			}
 
@@ -37,18 +60,26 @@ func ReadDir(dirpath string) []*Package {
 	return pkgs
 }
 
-func ReadMatchingName(dirpath, pkgname string) []*Package {
+// ReadMatchingName reads all packages with the given name in a directory,
+// recursing into subdirectories.
+//
+// Error handling is managed in the same way as in ReadDir.
+func ReadMatchingName(dirpath, pkgname string, ch chan<- error) []*Package {
 	var pkgs []*Package
 
 	filepath.Walk(dirpath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			log.Printf("Warning: %s\n", err)
+			if ch != nil {
+				ch <- err
+			}
 			return nil
 		}
 		if !info.Mode().IsDir() && strings.HasPrefix(path, pkgname) && HasPackageFormat(path) {
 			p, err := ReadPackage(path)
 			if err != nil {
-				log.Printf("Warning: %s\n", err)
+				if ch != nil {
+					ch <- err
+				}
 				return nil
 			}
 
@@ -63,21 +94,28 @@ func ReadMatchingName(dirpath, pkgname string) []*Package {
 	return pkgs
 }
 
-// Note that we do not recurse into subdirectories!
-func ReadMatchingNames(dirpath string, pkgnames []string) []*Package {
+// ReadMatchingNames reads all packages with one of the given names in a directory,
+// at the moment it does not recurse into subdirectories.
+//
+// Error handling is managed the same as in ReadDir.
+func ReadMatchingNames(dirpath string, pkgnames []string, ch chan<- error) []*Package {
 	var pkgs []*Package
 
 	for _, n := range pkgnames {
 		matches, err := filepath.Glob(filepath.Join(dirpath, n+"-*.pkg.tar.*"))
 		if err != nil {
-			log.Printf("Warning: cannot find package %s.\n", n)
+			if ch != nil {
+				ch <- errors.New("cannot find package " + n)
+			}
 			continue
 		}
 
 		for _, fp := range matches {
 			p, err := ReadPackage(fp)
 			if err != nil {
-				log.Printf("Warning: %s\n.", err)
+				if ch != nil {
+					ch <- err
+				}
 				continue
 			}
 
