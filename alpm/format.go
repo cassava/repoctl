7c1980eb04ca44301f2d22f2699aca32aa2bc3ea
Ben Morgan <neembi@gmail.com>
2015-10-18 17:46:27 +0200

    alpm: adding doc string

--- alpm/format.go
+++ alpm/format.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+// Package alpm works with parts of Arch Linux packages.
 package alpm
 
 import "strings"
