1d83d695302485ad698a0b678e850697dfb208a7
Ben Morgan <neembi@gmail.com>
2016-02-29 21:41:50 +0100

    Adding host/serve command

---
+++ cmd/repoctl/host.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2016, Ben Morgan. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+var listen string
+
+func init() {
+	RepoctlCmd.AddCommand(hostCmd)
+
+	hostCmd.Flags().StringVar(&listen, "listen", ":8080", "which address and port to listen on")
+}
+
+var hostCmd = &cobra.Command{
+	Use:     "host",
+	Aliases: []string{"serve"},
+	Short:   "host repository on a network",
+	Long: `Host the repository on a network.
+
+  This is essentially static file serving the repository on a specific
+  address and port, and is only meant for temporary use. If you want
+  to run something like this for longer, consider using darkhttpd.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Serving %q on %s...\n", Repo.Directory, listen)
+		err := http.ListenAndServe(listen, http.FileServer(http.Dir(Repo.Directory)))
+		dieOnError(err)
+	},
+}
