cassava/repoctl

commit c916cb5d10a4bcf502302a884c551f3cf1a076c0

Add (preliminary) handling of signature files
When adding, updating, backing up, and removing packages, repoctl
now first checks whether there is a signature and deals with it as
it would the package file itself (respectively adding, updating, etc.).

You can enforce the presence of signatures if you like:
- add learned -r, --require-signature option
- update learned -r, --require-signature option
- configuration file gained require_signature field

This should resolve issue #35.

actions.go
@@ -38,10 +38,24 @@ func (r *Repo) add(h errs.Handler, pkgfiles []string, ar func(string, string) er
 	}
 
 	added := make([]string, 0, len(pkgfiles))
-	for _, src := range pkgfiles {
-		dst := path.Join(r.Directory, path.Base(src))
-		r.printf("%s and adding to repository: %s\n", lbl, src)
-		err := ar(src, dst)
+	for _, f := range pkgfiles {
+		pkg, err := NewSignedPkg(f)
+		if err != nil {
+			// This means that we are trying to add something that's
+			// non-existant or corrupt.
+			r.errorf("skipping %s: %s", f, err)
+			continue
+		}
+		if r.RequireSignature && !pkg.HasSignature() {
+			r.errorf("skipping %s: require signature but none available", f)
+			continue
+		}
+
+		r.printf("%s and adding to repository: %s\n", lbl, pkg.PathSet())
+		err = pkg.Apply(func(src string, _ bool) error {
+			dst := path.Join(r.Directory, path.Base(src))
+			return ar(src, dst)
+		})
 		if err != nil {
 			err = h(err)
 			if err != nil {
@@ -49,7 +63,7 @@ func (r *Repo) add(h errs.Handler, pkgfiles []string, ar func(string, string) er
 			}
 			continue
 		}
-		added = append(added, dst)
+		added = append(added, path.Join(r.Directory, path.Base(f)))
 	}
 
 	err := r.DatabaseAdd(added...)
@@ -123,16 +137,28 @@ func (r *Repo) backup(h errs.Handler, pkgfiles []string) error {
 
 	backupDir := r.backupDirAbs()
 	for _, f := range pkgfiles {
-		src := path.Base(f)
-		r.printf("backing up: %s\n", f)
-		dst := path.Join(backupDir, src)
-		err := osutil.MoveFileLazy(f, dst)
+		pkg, err := NewSignedPkg(f)
 		if err != nil {
 			err = h(err)
 			if err != nil {
 				return err
 			}
-			continue
+			if pkg == nil {
+				continue
+			}
+		}
+
+		r.printf("backing up: %s\n", pkg.NameSet())
+		err = pkg.Apply(func(f string, _ bool) error {
+			src := path.Base(f)
+			dst := path.Join(backupDir, src)
+			return osutil.MoveFileLazy(f, dst)
+		})
+		if err != nil {
+			err = h(err)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
@@ -140,14 +166,27 @@ func (r *Repo) backup(h errs.Handler, pkgfiles []string) error {
 
 func (r *Repo) unlink(h errs.Handler, pkgfiles []string) error {
 	for _, f := range pkgfiles {
-		r.printf("deleting: %s\n", f)
-		err := os.Remove(f)
+		pkg, err := NewSignedPkg(f)
 		if err != nil {
 			err = h(err)
 			if err != nil {
 				return err
 			}
-			continue
+			if pkg == nil {
+				continue
+			}
+		}
+
+		r.printf("deleting: %s\n", pkg.NameSet())
+
+		err = pkg.Apply(func(f string, _ bool) error {
+			return os.Remove(f)
+		})
+		if err != nil {
+			err = h(err)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
@@ -177,11 +216,22 @@ func (r *Repo) Update(h errs.Handler, pkgnames ...string) error {
 			missing = append(missing, p.Name)
 			continue
 		}
-		if p.HasUpdate() || len(pkgnames) > 0 {
-			updates = append(updates, p.Pkg().Filename)
-		}
 		if p.HasObsolete() {
 			obsolete = append(obsolete, pu.Map(p.Obsolete(), pu.PkgFilename)...)
+		}
+		if p.HasUpdate() || len(pkgnames) > 0 {
+			f := p.Pkg().Filename
+			if r.RequireSignature {
+				spkg, err := NewSignedPkg(f)
+				if err != nil {
+					r.errorf("skipping %s: %s", f, err)
+					continue
+				} else if spkg.HasSignature() {
+					r.errorf("skipping %s: require signature but none found", f)
+					continue
+				}
+			}
+			updates = append(updates, f)
 		}
 	}
 
@@ -198,7 +248,7 @@ func (r *Repo) Update(h errs.Handler, pkgnames ...string) error {
 	return r.Dispatch(h, obsolete...)
 }
 
-// Reset deletes the repository database and readd all the packages.
+// Reset deletes the repository database and reads all the packages.
 // This is the same as unlinking the database and then running Update.
 func (r *Repo) Reset(h errs.Handler) error {
 	errs.Init(&h)

cmd/repoctl/add.go
@@ -7,11 +7,13 @@ package main
 import "github.com/spf13/cobra"
 
 var movePackages bool
+var addRequireSignature bool
 
 func init() {
 	MainCmd.AddCommand(addCmd)
 
 	addCmd.Flags().BoolVarP(&movePackages, "move", "m", false, "move packages into repository")
+	addCmd.Flags().BoolVarP(&addRequireSignature, "require-signature", "r", false, "require package signatures")
 }
 
 var addCmd = &cobra.Command{
@@ -35,10 +37,13 @@ var addCmd = &cobra.Command{
 `,
 	Example: `  repoctl add -m ./fairsplit-1.0.pkg.tar.gz`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if addRequireSignature {
+			Repo.RequireSignature = true
+		}
+
 		if movePackages {
 			return Repo.Move(nil, args...)
-		} else {
-			return Repo.Copy(nil, args...)
 		}
+		return Repo.Copy(nil, args...)
 	},
 }

cmd/repoctl/update.go
@@ -6,8 +6,12 @@ package main
 
 import "github.com/spf13/cobra"
 
+var updateRequireSignature bool
+
 func init() {
 	MainCmd.AddCommand(updateCmd)
+
+	updateCmd.Flags().BoolVarP(&updateRequireSignature, "require-signature", "r", false, "require package signatures")
 }
 
 var updateCmd = &cobra.Command{
@@ -30,6 +34,10 @@ var updateCmd = &cobra.Command{
 	Example: `  repoctl update fairsplit
   repoctl update --backup=false`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if updateRequireSignature {
+			Repo.RequireSignature = true
+		}
+
 		return Repo.Update(nil, args...)
 	},
 }

cmd/repoctl/version.go
@@ -39,8 +39,8 @@ var versionCmd = &cobra.Command{
 			Author:    "Ben Morgan",
 			Email:     "neembi@gmail.com",
 			Version:   "0.19",
-			Date:      "20 June, 2018",
-			Copyright: "2016-2018",
+			Date:      "28 April, 2019",
+			Copyright: "2016-2019",
 			Homepage:  "https://github.com/cassava/repoctl",
 			License:   "MIT",
 			Conf:      Conf,

conf/config.go
@@ -54,6 +54,10 @@ repo = {{printt .Repository}}
 # with AUR related tasks, such as determining if there is an update or not.
 #ignore_aur = {{printt .IgnoreAUR}}
 
+# require_signature prevents packages from being added that do not
+# also have a signature file.
+#require_signature = {{printt .RequireSignature}}
+
 # backup specifies whether package files should be backed up or deleted.
 # If it is set to false, then obsolete package files are deleted.
 backup = {{printt .Backup}}
@@ -108,6 +112,8 @@ type Configuration struct {
 	RemoveParameters []string `toml:"rm_params"`
 	// Packages to ignore when doing AUR related tasks.
 	IgnoreAUR []string `toml:"ignore_aur"`
+	// Require signatures for packages that are added to the database.
+	RequireSignature bool `toml:"require_signature"`
 
 	// Backup causes older packages to be backed up rather than deleted.
 	Backup bool `toml:"backup"`

conf/repo.go
@@ -22,6 +22,7 @@ func (c *Configuration) Repo() *repoctl.Repo {
 	r.IgnoreAUR = c.IgnoreAUR
 	r.AddParameters = c.AddParameters
 	r.RemoveParameters = c.RemoveParameters
+	r.RequireSignature = c.RequireSignature
 	r.Error = os.Stderr
 	if c.Quiet {
 		r.Info = nil

repo.go
@@ -24,6 +24,8 @@ type Repo struct {
 	// relative from Directory.
 	Database string
 
+	// RequireSignature specifies whether files can be added without signature.
+	RequireSignature bool
 	// Backup specifies whether to backup old packages.
 	Backup bool
 	// BackupDir specifies where old packages are backed up to.
@@ -148,4 +150,54 @@ func (r *Repo) debugf(format string, obj ...interface{}) {
 	if r.Debug != nil {
 		fmt.Fprintf(r.Debug, format, obj...)
 	}
+}
+
+// SignedPkg represents the path components of a potentially signed package.
+type SignedPkg struct {
+	PkgFile string
+	SigFile string
+}
+
+// NewSignedPkg returns a new SignedPkg, accompanied with an error if there
+// is an error reading whether the signature file exists or not.
+func NewSignedPkg(p string) (*SignedPkg, error) {
+	if ex, err := osutil.FileExists(p); !ex {
+		if err != nil {
+			return nil, err
+		}
+		return nil, &NotExistsError{p}
+	}
+	if ex, err := osutil.FileExists(p + ".sig"); ex {
+		return &SignedPkg{p, p + ".sig"}, err
+	}
+	return &SignedPkg{p, ""}, nil
+}
+
+// PathSet returns the path of the package file and the signature file.
+func (p *SignedPkg) PathSet() string {
+	if p.HasSignature() {
+		return p.PkgFile + "{,.sig}"
+	}
+	return p.PkgFile
+}
+
+// NameSet returns the basename of the package file and the signature file.
+func (p *SignedPkg) NameSet() string {
+	return path.Base(p.PathSet())
+}
+
+// HasSignature returns whether the package is accompanied by a signature file.
+func (p *SignedPkg) HasSignature() bool { return p.SigFile != "" }
+
+// Apply executes the function f for the package path, then the signature path.
+// It returns the first error encountered.
+func (p *SignedPkg) Apply(f func(string, bool) error) error {
+	err := f(p.PkgFile, false)
+	if err != nil {
+		return err
+	}
+	if p.HasSignature() {
+		return f(p.SigFile, true)
+	}
+	return nil
 }

commit dd7e5b3e137429d0121f772df48e475a3a265d7d

Fix regression from handling signature files
In the update command, update would skip files that had a signature
instead of skipping those that did not have a signature when the
--require-signature option was active.

This fixes an error introduced in c916cb5d.

actions.go
@@ -241,7 +241,7 @@ func (r *Repo) Update(h errs.Handler, pkgnames ...string) error {
 				if err != nil {
 					r.errorf("skipping %s: %s", f, err)
 					continue
-				} else if spkg.HasSignature() {
+				} else if !spkg.HasSignature() {
 					r.errorf("skipping %s: require signature but none found", f)
 					continue
 				}